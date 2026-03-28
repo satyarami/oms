e91689c2de29074b2ad55a29968fd8b60ecd6dd6
satya <satya.rami@gmail.com>
2026-03-28 10:42:14 -0400

    wip

--- scripts/stop-all.bat
+++ scripts/stop-all.bat
@@ -1,15 +1,15 @@
 @echo off
 REM Stop all 7 OMS applications by killing their console windows
 
-REM ── oms-core ──
+REM --- oms-core ---
 call "%~dp0stop-media-driver.bat"
 call "%~dp0stop-order-subscriber.bat"
 call "%~dp0stop-aeron-stat.bat"
 
-REM ── oms-ui ──
+REM --- oms-ui ---
 call "%~dp0stop-oms-ui.bat"
 
-REM ── oms-kdb ──
+REM --- oms-kdb ---
 call "%~dp0stop-aeron-listener.bat"
 call "%~dp0stop-kdb-writer.bat"
 call "%~dp0stop-queue-monitor.bat"
--- scripts/stop-order-subscriber.bat
+++ scripts/stop-order-subscriber.bat
@@ -1,3 +1,3 @@
-st@echo off
+@echo off
 REM Stop Order Subscriber by killing its console window using WMIC
 wmic process where "CommandLine like '%%Order Subscriber%%'" call terminate
